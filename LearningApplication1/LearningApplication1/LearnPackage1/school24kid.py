eec05c5671c06a4e643446421798899c2342bf48
frank.wang <frank.wang@anglicaresa.com.au>
2017-08-15 14:22:43 +0930

    School24 reading OK.

---
+++ LearningApplication1/LearningApplication1/LearnPackage1/school24kid.py
@@ -0,0 +1,65 @@
+"""
+Used for parse school24 website
+"""
+def main():
+    """
+        #main entrance for the app
+    """
+    import requests
+    import json
+    #from lxml import html
+    #from lxml import etree
+    from bs4 import BeautifulSoup
+    import pprint
+
+    #USERNAME = "<USERNAME>"
+    #PASSWORD = "<PASSWORD>"
+
+    login_url = "https://www.school24.net.au/ps_parent_dologin.asp"
+
+    content_url = "https://www.school24.net.au/ri/psn_oh.asp"
+
+    session_requests = requests.session()
+
+    # Get login csrf token
+    result = session_requests.get(login_url)
+
+    #Read username and password info from ini.json into payload
+    with open('ini.json') as json_data:
+        payload = json.load(json_data)
+
+    print('-----Username and password read OK.-----')
+
+
+    # Perform login
+    result = session_requests.post(
+        login_url, data=payload, headers=dict(referer=login_url))
+
+    if result:
+        print('-----Login OK.-----')
+
+    # get the latest 5 orders from content_url
+    result = session_requests.get(content_url, headers=dict(referer=content_url))
+    if result:
+        print('-----Get Orderlist OK.-----')
+    #parse to soup using beautiful soap
+    soup = BeautifulSoup(result.content.decode("utf-8"), "lxml")
+    #find the table that contains the list
+    table = soup.find("table", class_="w3-table w3-bordered w3-striped")
+    strs = '{'
+    for order_itme in table.find_all('tr', recursive=False):
+        col_name = ['DeliveryDate', ' Amount', ' Student', ' OrderID']
+        #strs = strs + '{'
+        for order_item_content in order_itme.find_all('td', limit=4, recursive=False):
+            strs = strs + '' + col_name.pop() + ':' + order_item_content.get_text() + ','
+        strs = strs.rstrip(',')
+        #strs = strs + '}'
+
+    strs = strs + '}'
+    pprint.pprint(strs)
+    #save to file
+    with open("Output.txt", "w") as text_file:
+        print(strs, file=text_file)
+
+if __name__ == '__main__':
+    main()
--- LearningApplication1/LearningApplication1/LearningApplication1.py
+++ LearningApplication1/LearningApplication1/LearningApplication1.py
@@ -1,37 +1,11 @@
-import requests
-from lxml import html
-
-USERNAME = "<USERNAME>"
-PASSWORD = "<PASSWORD>"
-
-LOGIN_URL = "https://www.school24.net.au/ps_parent_dologin.asp"
-
-URL = "https://www.school24.net.au/ri/psn_oh.asp"
-#URL = "https://www.school24.net.au/ri/ps.asp"
+"""
+Used for learning to wirte fully versioned python.
+"""
 def main():
-    session_requests = requests.session()
-
-    # Get login csrf token
-    result = session_requests.get(LOGIN_URL)
-    tree = html.fromstring(result.text)
-    #authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
-
-    # Create payload
-    payload = {
-        "username": "afeixiang@gmail.com", 
-        "password": "woshiafei"
-    }
-
-    # Perform login
-    result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
-
-    # Scrape url
-    result = session_requests.get(URL, headers = dict(referer = URL))
-    result = session_requests.get(URL, headers = dict(referer = URL))
-    tree = html.fromstring(result.content)
-    bucket_names = tree.xpath("/html")
-
-    print(tree.__str__)
-
+    """
+    used for turtle
+    """
+    import LearnPackage1.turtle as turtle
+    turtle.TurtleShell().cmdloop()
 if __name__ == '__main__':
     main()
