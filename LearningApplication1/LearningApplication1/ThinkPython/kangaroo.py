9c125b02ced9b9e81018b153dbe175be7c04a073
frank.wang <frank.wang@anglicaresa.com.au>
2017-10-12 16:30:36 +1030

    ex19_11.py added.

--- LearningApplication1/LearningApplication1/ThinkPython/kangaroo.py
+++ LearningApplication1/LearningApplication1/ThinkPython/kangaroo.py
@@ -21,23 +21,20 @@ you DO NOT want to emulate this example!
 
 class Kangaroo:
     """A Kangaroo is a marsupial."""
-    
-    def __init__(self, name, contents=[]):
+    def __init__(self, name, contents=None):
         """Initialize the pouch contents.
 
         name: string
         contents: initial pouch contents.
         """
         self.name = name
-        if len(contents) == 0:
-            self.pouch_contents = []
-        else:
-            self.pouch_contents = contents
+        self.pouch_contents = [] if contents is None else contents
+
 
     def __str__(self):
         """Return a string representaion of this Kangaroo.
         """
-        t = [ self.name + ' has pouch contents:' ]
+        t = [self.name + ' has pouch contents:']
         for obj in self.pouch_contents:
             s = '    ' + object.__str__(obj)
             t.append(s)
@@ -64,5 +61,4 @@ print('done!')
 
 # If you run this program as is, it seems to work.
 # To see the problem, trying printing roo.
-
 # Hint: to find the problem try running pylint.
